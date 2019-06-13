92a299bce10af182469e2b72a57b774d2ab10233
unknown <ruiz.james12@yahoo.com>
2019-06-13 14:09:38 -0700

    Add Lead Implemented

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../actions/leads";
 
 class Form extends Component {
   state = {
@@ -7,11 +10,17 @@ class Form extends Component {
     message: ""
   };
 
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  };
+
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
-    console.log("submitted");
+    const { name, email, message } = this.state;
+    const lead = { name, email, message };
+    this.props.addLead(lead);
   };
 
   render() {
@@ -61,4 +70,7 @@ class Form extends Component {
   }
 }
 
-export default Form;
+export default connect(
+  null,
+  { addLead }
+)(Form);
